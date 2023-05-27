a0fa45264570986cda943e348369e3a00f79c895
MishaSudnitsyn <59748018+MishaSudnitsyn@users.noreply.github.com>
2023-05-27 15:03:17 +0300

    feat(): update

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-}
-
 variable "aws_region" {
   description = "AWS Region"
   default = "eu-central-1"
